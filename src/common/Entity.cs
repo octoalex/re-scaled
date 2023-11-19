72f958ac41755e61f81b60d5217adebd466b413b
OctoAlex <123303292+octoalex@users.noreply.github.com>
2023-11-19 20:13:01 +0100

    Added Entities Interface & PlayerEntity

---
+++ src/common/Entity.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+namespace OctoAlex.ReScaled.Common;
+
+public interface IEntity {
+	public void Hit ( );
+}
---
+++ src/entities/player/PlayerEntity.cs
@@ -0,0 +1,10 @@
+using Godot;
+using OctoAlex.ReScaled.Common;
+
+namespace OctoAlex.ReScaled.Entities.Player;
+
+public partial class PlayerEntity : CharacterBody3D, IEntity {
+	public void Hit ( ) {
+		// code to game over
+	}
+}
