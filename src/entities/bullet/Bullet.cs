302b80645e29fce6b3800e87d3da1b27d0736013
OctoAlex <123303292+octoalex@users.noreply.github.com>
2023-12-21 22:33:17 +0100

    Updated bullet to properly function

--- src/entities/bullet/Bullet.cs
+++ src/entities/bullet/Bullet.cs
@@ -4,10 +4,16 @@ using OctoAlex.ReScaled.Common;
 
 namespace OctoAlex.ReScaled.Entities.Bullet;
 
-public partial class Bullet : Node3D {
+public partial class Bullet : RigidBody3D {
+
+	public Bullet ( ) {
+		BodyEntered += _OnBulletCollide;
+	}
+
 	private void _OnBulletCollide ( Node body ) {
 		if (body.IsInGroup("entity") && body is IEntity bodyEntity) {
 			bodyEntity.Hit();
 		}
+		QueueFree();
 	}
 }
