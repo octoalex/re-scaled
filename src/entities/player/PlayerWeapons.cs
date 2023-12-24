ff01ddc183efcb9b9c27a81c651a28788645e0d4
OctoAlex <123303292+octoalex@users.noreply.github.com>
2023-12-24 23:05:10 +0100

    Improved Deploy Logic

--- src/entities/player/PlayerWeapons.cs
+++ src/entities/player/PlayerWeapons.cs
@@ -22,16 +22,19 @@ public partial class PlayerWeapons : Node3D {
 
 	private int _active;
 
+	private bool _skipDeploy;
+
 	public override void _Ready ( ) {
 		_wEffective = new IWeapon[_weapons.Length];
 		_wNodes = new Node3D[_weapons.Length];
+		_skipDeploy = true;
 		for (int i = 0; i < _weapons.Length; i++) {
 			Node child = _weapons[i].Instantiate();
 			_wEffective[i] = child as IWeapon ?? throw new ArgumentException("Initial Weapon must implement IWeapon");
 			_wNodes[i] = child as Node3D;
 			AddChild(child);
 		}
-		
+		_skipDeploy = false;
 		Deploy(0);
 	}
 
@@ -55,9 +58,13 @@ public partial class PlayerWeapons : Node3D {
 		Array.Copy(_wNodes, bufferNode, _wNodes.Length);
 		bufferNode[_wNodes.Length] = child as Node3D;
 		_wNodes = bufferNode;
+		Deploy(_wNodes.Length - 1);
 	}
 
 	private void Deploy ( int next ) {
+		if (next == _active || _skipDeploy) {
+			return;
+		}
 		_wNodes[_active].Visible = false;
 		_wNodes[next].Visible = true;
 		_active = next;
